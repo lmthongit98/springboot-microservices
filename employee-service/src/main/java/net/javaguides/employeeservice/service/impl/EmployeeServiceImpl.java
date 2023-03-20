db524f232cce1d426a8fa4ef966ccc1634847937
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-03-20 22:08:29 +0700

    Employee api

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package net.javaguides.employeeservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.entity.Employee;
+import net.javaguides.employeeservice.mapper.EmployeeMapper;
+import net.javaguides.employeeservice.repository.EmployeeRepository;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
+    private EmployeeRepository employeeRepository;
+
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee saveDEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(saveDEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+}
